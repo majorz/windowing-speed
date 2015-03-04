79361098ce4e229f07c95f3871f71aea6e982901
zahari <zahari@polydraw.com>
2015-03-04 15:09:45 +0200

    Cleaning up dead code.

--- src/ffi/xcb.rs
+++ src/ffi/xcb.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 use libc::{c_int, c_char, c_uchar, c_ushort, c_uint};
 use std::ptr;
 use std::mem;
@@ -7,76 +5,16 @@ use std::default::Default;
 
 // CONSTANTS
 
-const XCB_WINDOW_CLASS_COPY_FROM_PARENT: c_ushort = 0;
 const XCB_WINDOW_CLASS_INPUT_OUTPUT: c_ushort = 1;
-const XCB_WINDOW_CLASS_INPUT_ONLY: c_ushort = 2;
 
-const XCB_GC_FUNCTION: c_uint = 1;
-const XCB_GC_PLANE_MASK: c_uint = 2;
 const XCB_GC_FOREGROUND: c_uint = 4;
-const XCB_GC_BACKGROUND: c_uint = 8;
-const XCB_GC_LINE_WIDTH: c_uint = 16;
-const XCB_GC_LINE_STYLE: c_uint = 32;
-const XCB_GC_CAP_STYLE: c_uint = 64;
-const XCB_GC_JOIN_STYLE: c_uint = 128;
-const XCB_GC_FILL_STYLE: c_uint = 256;
-const XCB_GC_FILL_RULE: c_uint = 512;
-const XCB_GC_TILE: c_uint = 1024;
-const XCB_GC_STIPPLE: c_uint = 2048;
-const XCB_GC_TILE_STIPPLE_ORIGIN_X: c_uint = 4096;
-const XCB_GC_TILE_STIPPLE_ORIGIN_Y: c_uint = 8192;
-const XCB_GC_FONT: c_uint = 16384;
-const XCB_GC_SUBWINDOW_MODE: c_uint = 32768;
 const XCB_GC_GRAPHICS_EXPOSURES: c_uint = 65536;
-const XCB_GC_CLIP_ORIGIN_X: c_uint = 131072;
-const XCB_GC_CLIP_ORIGIN_Y: c_uint = 262144;
-const XCB_GC_CLIP_MASK: c_uint = 524288;
-const XCB_GC_DASH_OFFSET: c_uint = 1048576;
-const XCB_GC_DASH_LIST: c_uint = 2097152;
-const XCB_GC_ARC_MODE: c_uint = 4194304;
 
-const XCB_CW_BACK_PIXMAP: c_uint = 1;
 const XCB_CW_BACK_PIXEL: c_uint = 2;
-const XCB_CW_BORDER_PIXMAP: c_uint = 4;
-const XCB_CW_BORDER_PIXEL: c_uint = 8;
-const XCB_CW_BIT_GRAVITY: c_uint = 16;
-const XCB_CW_WIN_GRAVITY: c_uint = 32;
-const XCB_CW_BACKING_STORE: c_uint = 64;
-const XCB_CW_BACKING_PLANES: c_uint = 128;
-const XCB_CW_BACKING_PIXEL: c_uint = 256;
-const XCB_CW_OVERRIDE_REDIRECT: c_uint = 512;
-const XCB_CW_SAVE_UNDER: c_uint = 1024;
 const XCB_CW_EVENT_MASK: c_uint = 2048;
-const XCB_CW_DONT_PROPAGATE: c_uint = 4096;
-const XCB_CW_COLORMAP: c_uint = 8192;
-const XCB_CW_CURSOR: c_uint = 16384;
 
-const XCB_EVENT_MASK_NO_EVENT: c_uint = 0;
 const XCB_EVENT_MASK_KEY_PRESS: c_uint = 1;
-const XCB_EVENT_MASK_KEY_RELEASE: c_uint = 2;
-const XCB_EVENT_MASK_BUTTON_PRESS: c_uint = 4;
-const XCB_EVENT_MASK_BUTTON_RELEASE: c_uint = 8;
-const XCB_EVENT_MASK_ENTER_WINDOW: c_uint = 16;
-const XCB_EVENT_MASK_LEAVE_WINDOW: c_uint = 32;
-const XCB_EVENT_MASK_POINTER_MOTION: c_uint = 64;
-const XCB_EVENT_MASK_POINTER_MOTION_HINT: c_uint = 128;
-const XCB_EVENT_MASK_BUTTON_1_MOTION: c_uint = 256;
-const XCB_EVENT_MASK_BUTTON_2_MOTION: c_uint = 512;
-const XCB_EVENT_MASK_BUTTON_3_MOTION: c_uint = 1024;
-const XCB_EVENT_MASK_BUTTON_4_MOTION: c_uint = 2048;
-const XCB_EVENT_MASK_BUTTON_5_MOTION: c_uint = 4096;
-const XCB_EVENT_MASK_BUTTON_MOTION: c_uint = 8192;
-const XCB_EVENT_MASK_KEYMAP_STATE: c_uint = 16384;
 const XCB_EVENT_MASK_EXPOSURE: c_uint = 32768;
-const XCB_EVENT_MASK_VISIBILITY_CHANGE: c_uint = 65536;
-const XCB_EVENT_MASK_STRUCTURE_NOTIFY: c_uint = 131072;
-const XCB_EVENT_MASK_RESIZE_REDIRECT: c_uint = 262144;
-const XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY: c_uint = 524288;
-const XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT: c_uint = 1048576;
-const XCB_EVENT_MASK_FOCUS_CHANGE: c_uint = 2097152;
-const XCB_EVENT_MASK_PROPERTY_CHANGE: c_uint = 4194304;
-const XCB_EVENT_MASK_COLOR_MAP_CHANGE: c_uint = 8388608;
-const XCB_EVENT_MASK_OWNER_GRAB_BUTTON: c_uint = 16777216;
 
 
 // TYPES & STRUCTURES
@@ -367,21 +305,5 @@ impl XCB {
             Some(_) => ()
         }
     }
-
-    pub fn print_screen_dimensions(&self) {
-        match self.screen {
-            Some(screenp) => unsafe {
-                let screen = *screenp;
-                println!(
-                    "Screen w: {}, h: {}",
-                    screen.width_in_pixels,
-                    screen.height_in_pixels
-                );
-            },
-            None => ()
-        }
-    }
 }
-
-
 
